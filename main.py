a89a5a60dff29ca7b5605dae2b830246ece50652
Norbert <kieler103@gmail.com>
2024-03-24 13:36:40 +0100

    medium

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import palindrome
-import roman_to_integer, longest_prefix
+import roman_to_integer, longest_prefix, valid_parentheses
 
 # print(palindrome.isPalindrome(333))
 # print(roman_to_integer.roman_to_int("DMI"))
-print(longest_prefix.longestCommonPrefix(["flowerrrrr", "flower", "flower"]))
-
+# print(longest_prefix.longestCommonPrefix(["flowerrrrr", "flower", "flower"]))
+print(valid_parentheses.is_valid("()"))
---
+++ valid_parentheses.py
@@ -0,0 +1,23 @@
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is
+# valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+def is_valid(s):
+    stack = []
+    for char in s:
+        if char in '([{':
+            stack.append(char)
+        else:
+            if not stack or \
+                    (char == ')' and stack[-1] != '(') or \
+                    (char == '}' and stack[-1] != '{') or \
+                    (char == ']' and stack[-1] != '['):
+                return False
+            stack.pop()
+    return not stack
